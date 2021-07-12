72da2e99aa061d4d8d496c35e2c82d5e63edaeb4
elminhoemmanuel <amodue15@gmail.com>
2021-07-12 13:50:39 -0600

    added redux persist to retain todos

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import { Provider } from 'react-redux'
-import store from '../redux/store'
+import { store } from '../redux/store'
+import { persistor } from '../redux/store'
 import { createWrapper } from 'next-redux-wrapper'
 import '../styles/globals.css'
+import { PersistGate } from 'redux-persist/integration/react'
 
  function App({ Component, pageProps }) {
   
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   )
 }
--- redux/store.js
+++ redux/store.js
@@ -2,12 +2,19 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from "redux-devtools-extension";
 import logger from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import {rootReducer} from './reducers'
 
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const middleware = [thunk];
+export const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(...middleware, logger)))
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware, logger)))
-
-export default store
+export const persistor = persistStore(store);
